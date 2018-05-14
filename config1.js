d0c6dc0d321226735b79e52c16d4b77b89c749bc
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-05-14 11:50:12 +0200

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NDI1MDAxNjc3OTQzMjA5OTg0.DcbVjg.cMJ9gvo21R9auOHX3pVZqu7ZhOU'; // حط توكن البوت هنا
+exports.TOKEN = process.env.BOT_TOKEN; 
 
-exports.PREFIX = '+';//برفكس البوت هو يلي بيشغل لاغاني
+exports.PREFIX = '+';
 
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لا تغيره
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; 
