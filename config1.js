c0f7fc531672a5b5921aba6bcf3a3375c1963bdc
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-05-14 11:49:11 +0200

    Add files via upload

---
+++ config1.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NDI1MDAxNjc3OTQzMjA5OTg0.DcbVjg.cMJ9gvo21R9auOHX3pVZqu7ZhOU'; // حط توكن البوت هنا
+
+exports.PREFIX = '+';//برفكس البوت هو يلي بيشغل لاغاني
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لا تغيره
